18bb73c18f589a59021dfa1d5b15568f60c04394
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-12-11 16:09:16 +1100

    recover time + next available

--- R code/Integrated_Solution_ver1.3.R
+++ R code/Integrated_Solution_ver1.3.R
@@ -4,28 +4,12 @@
 # a: current_rating
 # dt: required training time to restore
 # di: duration
-# elf_score <- function (a, di){
-#     da <- -1440*log(4*a)/(log(1.02)+log(0.9))
-#     if (di < 600 * a){
-#         dt <- 0 # < 600 * a
-#     }else if(di < da){
-#         dt <- a * (1.02^(10 * di / 24 * a)) * (0.9^(14 * di / 24 * a)) * 60 * exp(-di*(10*log(1.02) + 14*log(0.9))/(a * 1440)-1) # [a * 600, da)
-#     }else{
-#         dt <- 60 * (4*a -1) / (4 * log(1.02)) # >=da
-#     }
-#     return(dt)
-# }
-   
 assign_elf <- function(myelves, di) {
     a <- myelves[,'current_rating']
     myelves[,'score'] <- 60 * (4*a -1) / (4 * log(1.02)) # >=da
-    
     myelves[di < -1440*log(4*a)/(log(1.02)+log(0.9)),'score'] <- a[di < -1440*log(4*a)/(log(1.02)+log(0.9))] * (1.02^(10 * di / 24 * a[di < -1440*log(4*a)/(log(1.02)+log(0.9))])) * (0.9^(14 * di / 24 * a[di < -1440*log(4*a)/(log(1.02)+log(0.9))])) * 60 * exp(-di*(10*log(1.02) + 14*log(0.9))/(a[di < -1440*log(4*a)/(log(1.02)+log(0.9))] * 1440)-1) # [a * 600, da)
-    
     myelves[di < myelves[,'current_rating'] * 600 ,'score'] <- 0 # < 600 * a
-    
     assigned_elf <-as.integer(myelves[order(myelves[,'score'], myelves[,'next_available_time']) ,'elf_id'][1])
-    
     return(assigned_elf)
 }
 
