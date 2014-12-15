f3d71820155218fb6da9d7fd7aaddc7f7368f7b1
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-12-15 22:30:59 +1100

    ref

---
+++ Simulates_annealing.R
@@ -0,0 +1,5 @@
+# Use SA to separately optimise each elf's workflow.
+# A reasonable starting distribution of toys
+# Recalculate the elf's total time(cost function) on each change
+# e.g.
+# from arbitrary sequence of toy data, calculate end time for processing them in strict order
