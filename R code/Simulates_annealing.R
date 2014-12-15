027bed9f2dae0414ff1bb16491af2bb3a672e1fa
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-12-16 10:07:12 +1100

    SAs

--- R code/Simulates_annealing.R
+++ R code/Simulates_annealing.R
@@ -27,4 +27,60 @@
 #     endDo S
 # endDo C
 # Step 6. Explore the Neighbourhood of the best solution
+
+
+#############
+### Setup ###
+#############
+setwd('/Users/ivan/Work_directory/FICO/Helping-Santas-Helpers/')
+setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/FICO/Helping-Santas-Helpers')
+setwd('H:/Machine_Learning/FICO/Helping-Santas-Helpers')
+gc(); rm(list=ls())
+source('R code/Functions.R')
+load('data/toys.RData')
+
+
+###################
+### Calculation ###
+###################
+solution_sortedElf <- function(myToys, myelves){
+    cat(format(Sys.time(),format = '%Y-%m-%d %H:%M:%S'))
+    outcomes <- matrix(0, nrow = nrow(myToys), ncol = 5, 
+                       dimnames = list(NULL, c('ToyId', 'ElfId', 'StartTime', 'Duration', 'current_rating')))
     
+    for(current_toy in 1:nrow(myToys)){
+        
+        # train_elf <- 1.0; overwork_elf <- 3.9
+        myelves[myelves[,'current_rating']>overwork_elf,'score'] <- 3
+        myelves[myelves[,'current_rating']<=overwork_elf,'score'] <- 2
+        myelves[myelves[,'current_rating']<=train_elf,'score'] <- 1
+        
+        c_toy_id <- assign_toy(myelves, myToys)
+        c_toy_arrival <- myToys[c_toy_id, 'Arrival_time'] 
+        c_toy_duration <- myToys[c_toy_id,'Duration']
+        c_toy_size <- myToys[c_toy_id,'Size']
+        
+        next_elf <- assign_elf(myelves, c_toy_duration, c_toy_size)
+        
+        c_elf_id <- myelves[next_elf, 'elf_id']
+        c_elf_start_time <- myelves[next_elf, 'next_available_time']
+        c_elf_rating <- myelves[next_elf, 'current_rating']
+        
+        if(c_elf_start_time < c_toy_arrival) c_elf_start_time <- c_toy_arrival    
+        
+        work_duration <- as.integer(ceiling(c_toy_duration/c_elf_rating))
+        
+        myelves[next_elf, 'next_available_time'] <- update_next_available_minute(c_elf_start_time, work_duration)
+        
+        myelves[next_elf, 'current_rating'] <- update_productivity(c_elf_start_time, work_duration, c_elf_rating)
+        
+        outcomes[current_toy,] <- c(c_toy_id, c_elf_id, c_elf_start_time, work_duration, c_elf_rating)
+        
+        myToys <- myToys[-c_toy_id, ]
+        
+        if(current_toy %% 100000 == 0) cat('\nCompleted', current_toy/1000000, 'mil toys, makespan', c_elf_start_time, 'minutes',
+                                           format(Sys.time(),format = '%Y-%m-%d %H:%M:%S'))    
+    }
+    cat('\nCompleted 10 mil toys at', convert_to_chardate(c_elf_start_time)) 
+    return(outcomes)
+}
