9bf59e99e4c1a983984eaf5d7c57ef1c880c4a0d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-12-22 15:45:27 +1100

    c++ model

--- R code/Integrated_Solution_ver1.7.R
+++ R code/Integrated_Solution_ver1.7.R
@@ -13,6 +13,10 @@ NUM_ELVES <- 900
 myelves <- create_elves(NUM_ELVES)
 myelves_rate <- myelves[,'current_rating']
 
+### Change ###
+toys <- toys[order(toys[,3]),]
+
+### Model ###
 submissions <- solution_Elf(toys, myelves,myelves_rate)
 
 (submissions[which.max(submissions[,3]),3]+submissions[which.max(submissions[,3]), 4])*log(901)
@@ -22,3 +26,6 @@ submissions_output <- data.frame(ToyId = as.integer(submissions[,1]),
                                  StartTime = convert_to_chardate(submissions[,3]), 
                                  Duration = as.integer(submissions[,4]), stringsAsFactors = FALSE)
 write.csv(submissions_output, 'toys_submission_classification_sort.csv', row.names = FALSE)
+
+# toys[,2&3] => 1855907408
+# toys[,2] => 1875750845
