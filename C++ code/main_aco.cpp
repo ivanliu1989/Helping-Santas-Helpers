11df0729b414e2d7356d6870007ec73f7fb2a43f
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-12-23 11:20:13 +1100

    ant colony opitimization

---
+++ C++ code/main_aco.cpp
@@ -0,0 +1,9 @@
+for(int ant=0;ant<50;ant++){
+	for(int current_toy=0;current_toy<10000000;current_toy++){
+		select the processor
+		stochastically using the τ matrix
+	}
+}
+If the schedule is feasible, compute its quality. 
+Update the pheromone based on the quality of each feasible schedule 
+Generate the τ matrix for the next iteration
