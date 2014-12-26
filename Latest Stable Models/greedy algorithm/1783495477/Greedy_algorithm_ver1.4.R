9712dc787200a3266695d75ce07ff917915b10b3
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-12-27 10:48:18 +1100

    update

---
+++ Latest Stable Models/greedy algorithm/1783495477/Greedy_algorithm_ver1.4.R
@@ -0,0 +1,51 @@
+#setwd('C:/Users/Ivan.Liuyanfeng/Desktop/Data_Mining_Work_Space/FICO/Helping-Santas-Helpers')
+#gc(); rm(list=ls()); source('R code/Functions.R');
+require(Rcpp)
+sourceCpp('Latest Stable Models/greedy algorithm/main_greedy_4.cpp')
+
+### Segmentation Elf ###
+NUM_ELVES <- 900
+myelves <- create_elves(NUM_ELVES)
+myelves_rate <- myelves[,'current_rating']
+
+### main loop ###
+submissions <- solution_Elf(toys_0,toys_1,toys_2,toys_3,toys_4,toys_5,toys_6,toys_7,toys_8,
+                            toys_9,toys_10,toys_11,toys_12,toys_13,toys_14,toys_15,toys_16,
+                            toys_17,myelves,myelves_rate)
+(submissions[which.max(submissions[,3]),3]+submissions[which.max(submissions[,3]), 4])*log(901)
+
+length(table(submissions[,1]));length(table(submissions[,2]))
+
+submissions_output <- data.frame(ToyId = as.integer(submissions[,1]), 
+                                 ElfId = as.integer(submissions[,2]), 
+                                 StartTime = convert_to_chardate(submissions[,3]), 
+                                 Duration = as.integer(submissions[,4]), stringsAsFactors = FALSE)
+write.csv(submissions_output, 'toys_submission_greedy_algorithm_1_2.csv', row.names = FALSE)
+
+
+### start from 9.00 am, start time => proper toys dataset ###
+
+### maintain skill at 4 set(time)
+
+### decide circle => 2848(penalty free) (first arrive first work)
+
+### end circle in an exhausion work
+
+### set aside 14 days tasks to retrain
+
+### assign remaining tasks before the exhausion tasks
+
+### only fully train 1/2 longest exhausion tasks | train first 1/2 exhausion tasks
+
+### 1. circle aside <= different chunks of tasks
+
+# 1782626492 (4)
+
+# 1788308181
+# 1783495477
+
+x_all <- list()
+for (i in 1:900){
+    x_all[[i]] <- submissions[which(submissions[,2]==i), 1]
+}
+save(x_all, file='Latest Stable Models/simulated_annealing/greedy_algorithm_solution.RData')
--- Latest Stable Models/greedy algorithm/logical_path.cpp
+++ Latest Stable Models/greedy algorithm/logical_path.cpp
@@ -52,3 +52,38 @@ if((c_elf_rating >= 3.28) & (toy_row(17) < myToys_17.nrow())){
         toy_row(17) += 1;
     }
 }
+
+//New
+if((c_elf_rating == 4.0) & (toy_row(1) < myToys_1.nrow())){
+    
+    int c_elf_start_time_2 = std::max(c_elf_start_time, (int)myToys_1(toy_row(1),1)); //toy1 start time 
+    int act_duration = ceil(myToys_1(toy_row(1),2)/c_elf_rating); //toy1 actual work time 
+    int sanc = getSanctionedBreakdown(c_elf_start_time_2, act_duration); //toy1 sanctional time
+    double sanc_rate = sanc/act_duration;
+    Rcpp::Rcout << '\n' << sanc_rate;
+    if(sanc_rate <= 0.83){
+        
+        int c_elf_start_time_3 = std::max(c_elf_start_time, (int)myToys_0(toy_row(0),1)); //trival start time 
+        int act_duration_2 = ceil(myToys_0(toy_row(0),2)/c_elf_rating); //trival actual work time 
+        int sanc_2 = getSanctionedBreakdown(c_elf_start_time_3, act_duration); //trival sanctional time
+        double sanc_rate_2 = sanc_2/act_duration_2;
+        
+        if((toy_row(0) < myToys_0.nrow()) && (sanc_rate_2 >= 0.95)){
+            c_toy_id = myToys_0(toy_row(0),0);
+            c_toy_arrival = myToys_0(toy_row(0),1);
+            c_toy_duration = myToys_0(toy_row(0),2);
+            toy_row(0) += 1;
+        }else{
+            c_elf_start_time = 840 + sanc + c_elf_start_time_2;
+            c_toy_id = myToys_1(toy_row(1),0);
+            c_toy_arrival = myToys_1(toy_row(1),1);
+            c_toy_duration = myToys_1(toy_row(1),2);
+            toy_row(1) += 1;   
+        }
+    }else{
+        c_toy_id = myToys_1(toy_row(1),0);
+        c_toy_arrival = myToys_1(toy_row(1),1);
+        c_toy_duration = myToys_1(toy_row(1),2);
+        toy_row(1) += 1; 
+    }
+}
